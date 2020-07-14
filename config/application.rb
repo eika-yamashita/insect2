6d7df92a08bd12f65b15243ef2822c73625378b7
eika-yamashita <skdc0106@gmail.com>
2020-07-14 16:55:14 +0900

    a

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,10 @@ module Insect2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 end
-config.assets.initialize_on_precompile = false
